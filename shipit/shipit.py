fe370e45d7f834df6ab757e8ed4938c2a213b153
Jefferson Lizarzabal <jlizarzabal@firesoft.com.ve>
2018-02-27 15:29:16 -0400

    working in request method

--- shipit/shipit.py
+++ shipit/shipit.py
@@ -2,13 +2,16 @@ from __future__ import absolute_import
 import json
 import requests
 
-from .exceptions import EndpointNotFoundException
+from .exceptions import EndpointNotFoundException, AttributeNotValidException, ConnectException, EmailNotFoundException, \
+    NumberNotValidException, TokenNotFoundException
 
 
 class Shipit:
     ENV_DEVELOPMENT = 'development'
     ENV_PRODUCTION = 'production'
-    endpoint = 'http://api.shipit.cl/v/'
+    METHOD_POST = 'post'
+    METHOD_PUT = 'put'
+    base_api = 'http://api.shipit.cl/v/'
 
     email = None
     token = None
@@ -28,7 +31,7 @@ class Shipit:
                    'X-Shipit-Access-Token': self.token,
                    'Accept': self.token,
                    }
-        api_url = '{0}communeas'.format(self.endpoint)
+        api_url = '{0}communeas'.format(self.base_api)
 
         response = requests.get(api_url, headers=headers)
         if response.status_code == 404:
@@ -36,6 +39,19 @@ class Shipit:
 
         return json.loads(response.content.decode('utf-8'))
 
-    # def get(self):
-    #     if self.token == None:
-    #         raise
+    def request(self, method, endpoint):
+        if not self.token:
+            raise TokenNotFoundException
+        if not self.email:
+            raise EmailNotFoundException
+        endpoint = '{0}{1}'.format(self.base_api, endpoint)
+        data ={
+
+        }
+        headers = {'Content-Type': 'application/json',
+                   'X-Shipit-Email': self.email,
+                   'X-Shipit-Access-Token': self.token,
+                   'Accept': 'application/vnd.shipit.v2',
+                   }
+        if method in [self.METHOD_POST, self.METHOD_PUT]:
+            environment = self.ENV_PRODUCTION
