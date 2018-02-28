50dd0abf9f448a3faf366a0ca21b189bd8d23001
Jefferson Lizarzabal <dvjefferson@gmail.com>
2018-02-27 23:07:57 -0400

    added tests

---
+++ tests/test_shipit.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+import unittest
+from ..shipit.shipit import Shipit
+
+
+class TestShipitMethods(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.shipit = Shipit('jlizarzabal@firesoft.com.ve', 'miRxLALSLWWv9fUC7KyW')
+
+    def test_not_email_not_token(self):
+        shipit = Shipit('asdf', 'asdf')
+        shipit.email = None
+        shipit.token = None
+        regions = self.shipit.regions()
+        self.assertIsNotNone(regions)
+        self.assertNotIn('error', regions)
+
+    def test_regions(self):
+        regions = self.shipit.regions()
+        self.assertIsNotNone(regions)
+        self.assertNotIn('error', regions)
+
+    def test_communes(self):
+        communes = self.shipit.communes()
+        self.assertIsNotNone(communes)
+        self.assertNotIn('error', communes)
