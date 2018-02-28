373fc78307950f517f098cff4559725f62c1f30d
Jefferson Lizarzabal <dvjefferson@gmail.com>
2018-02-27 23:47:11 -0400

    working in imports

--- tests/test_shipit.py
+++ tests/test_shipit.py
@@ -1,16 +1,16 @@
 import unittest
-from ..shipit.shipit import Shipit
+from ..shipit import shipit
 
 
 class TestShipitMethods(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.shipit = Shipit('jlizarzabal@firesoft.com.ve', 'miRxLALSLWWv9fUC7KyW')
+        cls.shipit = shipit.Shipit('jlizarzabal@firesoft.com.ve', 'miRxLALSLWWv9fUC7KyW')
 
     def test_not_email_not_token(self):
-        shipit = Shipit('asdf', 'asdf')
-        shipit.email = None
-        shipit.token = None
+        ship = shipit.Shipit('asdf', 'asdf')
+        ship.email = None
+        ship.token = None
         regions = self.shipit.regions()
         self.assertIsNotNone(regions)
         self.assertNotIn('error', regions)
